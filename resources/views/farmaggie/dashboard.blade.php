f3b8684179d2cedf570bcc92ef9c78b57c5ff0f2
Natthanun Puntanee <ravenholm@windowslive.com>
2017-06-17 01:47:15 +0700

    Add hog's food price and change look&feel of this page

--- resources/views/farmaggie/dashboard.blade.php
+++ resources/views/farmaggie/dashboard.blade.php
@@ -13,7 +13,7 @@
 									<i class="fa fa-circle-o-notch fa-spin fa-3x" style="color:white"></i>
 								</div>
 								<div class="comments-info likes-info">
-									<h3>2</h3>
+									<h3>1</h3>
 									<a href="#">จำนวนฟาร์ม</a>
 								</div>
 								<div class="clearfix"> </div>
@@ -25,7 +25,7 @@
 									<i class="fa fa-heartbeat fa-3x" style="color:white"></i>
 								</div>
 								<div class="comments-info">
-									<h3>696</h3>
+									<h3>248</h3>
 									<a href="#">จำนวนสุกร</a>
 								</div>
 								<div class="clearfix"> </div>
@@ -34,11 +34,11 @@
 						<div class="col-md-3 top-comment-grid">
 							<div class="comments tweets">
 								<div class="comments-icon">
-									<i class="fa fa-btc fa-3x" style="color:white"></i>
+									<i class="fa fa-umbrella fa-3x" style="color:white"></i>
 								</div>
 								<div class="comments-info tweets-info">
-									<h3>1,960 </h3>
-									<a href="#">ราคาสุกรวันนี้</a>
+									<h3>29°C </h3>
+									<a href="#">สภาพอากาศ</a>
 								</div>
 								<div class="clearfix"> </div>
 							</div>
@@ -49,8 +49,61 @@
 									<i class="fa fa-tachometer fa-3x" style="color:white"></i>
 								</div>
 								<div class="comments-info views-info">
-									<h3>ดี</h3>
+									<h3>ดี:#1</h3>
 									<a href="#">คุณภาพฟาร์ม</a>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="social grid">
+					<div class="grid-info">
+						<div class="col-md-3 top-comment-grid">
+							<div class="comments views">
+								<div class="comments-icon">
+									<i class="fa fa-btc fa-3x" style="color:white"></i>
+								</div>
+								<div class="comments-info views-info">
+									<h3>1,960</h3>
+									<a href="#">ราคาสุกรมีชีวิตหน้าฟาร์ม</a>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+						<div class="col-md-3 top-comment-grid">
+							<div class="comments tweets">
+								<div class="comments-icon">
+									<i class="fa fa-btc fa-3x" style="color:white"></i>
+								</div>
+								<div class="comments-info tweets-info">
+									<h3>748</h3>
+									<a href="#">ราคาอาหารสุกรเล็ก (ต่อ 30 กก.)</a>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+						<div class="col-md-3 top-comment-grid">
+							<div class="comments">
+								<div class="comments-icon">
+									<i class="fa fa-btc fa-3x" style="color:white"></i>
+								</div>
+								<div class="comments-info">
+									<h3>716 </h3>
+									<a href="#">ราคาอาหารสุกรรุ่น (ต่อ 30 กก.)</a>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+						<div class="col-md-3 top-comment-grid">
+							<div class="comments likes">
+								<div class="comments-icon">
+									<i class="fa fa-btc fa-3x" style="color:white"></i>
+								</div>
+								<div class="comments-info likes-info">
+									<h3>693</h3>
+									<a href="#">ราคาอาหารสุกรเนื้อ (ต่อ 30 กก.)</a>
 								</div>
 								<div class="clearfix"> </div>
 							</div>
@@ -109,12 +162,12 @@
 								}, {
 								label: "รายรับ",
 								type: "bar",
-								backgroundColor: "rgba(0,0,0,0.2)",
+								backgroundColor: "rgba(164, 202, 200, 0.8)",
 								data: [12000,5000,6750,7340,15000,11100],
 								}, {
 								label: "รายจ่าย",
 								type: "bar",
-								backgroundColor: "rgba(0,0,0,0.2)",
+								backgroundColor: "rgba(233, 129, 103, 0.8)",
 								backgroundColorHover: "#3e95cd",
 								data: [20000,15000,7830,2470,1110,9870]
 								}
@@ -138,7 +191,7 @@
 				<div class="col-md-12 agile-bottom-right">
 					<div class="agile-bottom-grid">
 						<div class="area-grids-heading">
-							<h3>ยอดการขายสุกรย้อนหลัง 6 เดือน</h3>
+							<h3>ยอดการขายสุกรย้อนหลัง 6 เดือน (ตัว)</h3>
 						</div>
 						<div id="graph9"></div>
 						<script>
@@ -148,7 +201,7 @@
 							{"elapsed": "มีนาคม", "value": 36},
 							{"elapsed": "เมษายน", "value": 79},
 							{"elapsed": "พฤษภาคม", "value": 48},
-							{"elapsed": "มิถานายน", "value": 78}
+							{"elapsed": "มิถุนายน", "value": 78}
 							];
 							Morris.Line({
 								element: 'graph9',
